697a462665c203f3a8aa9fa249df19987a2ddc4c
sodiq yusuff <ysodiqakanni@gmail.com>
2023-07-18 16:33:13 -0500

    create cities controller

---
+++ transit-api/Controllers/CitiesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace transit_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllCities()
+        {
+            var list = new List<object>()
+            {
+                new
+                {
+                    id = 1,
+                    name = "Houston"
+                },
+                new
+                {
+                    id = 2,
+                    name = "Austin"
+                },
+                new
+                {
+                    id = 3,
+                    name = "Dallas"
+                }
+            };
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCityById(int id)
+        {
+            return Ok(new { 
+                id,
+                name = "Tampa"
+            });
+        }
+
+    }
+}
